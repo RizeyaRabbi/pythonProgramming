bbd489b193e29400cacf63000b9d2a3aaa2b9ce9
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-10 15:03:21 +0600

    commit from office

---
+++ lambdaFunction.py
@@ -0,0 +1,14 @@
+# Python lambda function
+# small anonymous function
+# can take any number of arguments,can have only one expression
+# lambda is used when anonymus function ios required for short period of time
+'''
+def func(x):
+    return lambda a: a*x
+
+
+double = func(2)
+tripple = func(3)
+print(double(5))
+print(tripple(5))
+'''
