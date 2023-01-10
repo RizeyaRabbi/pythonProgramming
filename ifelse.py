b1ac389f109c9a54e1aa5a2f61114e91202b5b0c
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-10 11:32:38 +0600

    commit from office

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+# Python if else
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+a = 30
+b = 50
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal b")
+else:
+    print("a is greater than b")
