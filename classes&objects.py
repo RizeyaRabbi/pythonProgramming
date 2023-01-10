72820a2ecfa6dd325400288bf9221743bb221a8f
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-10 17:06:41 +0600

    commit from office

--- classes&objects.py
+++ classes&objects.py
@@ -1,1 +1,29 @@
 # Python Classes and Objects
+'''
+class testClass:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sum(self):
+        print(self.num1+self.num2)
+
+
+objectOfClass0 = objectOfClass1 = objectOfClass2 = testClass(5, 6)
+objectOfClass0.sum()
+objectOfClass1.sum()
+objectOfClass2.sum()
+'''
+
+'''
+class testClass:
+    def __init__(self):
+        pass
+
+    def sum(self, num1, num2):
+        print(num1+num2)
+
+
+obj = testClass()
+obj.sum(5, 6)
+'''
