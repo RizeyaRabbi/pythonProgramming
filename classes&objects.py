cd342e635b92316f99914295ad2c200ebc46068b
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-11 10:41:11 +0600

    commit from office

--- classes&objects.py
+++ classes&objects.py
@@ -28,10 +28,11 @@ obj = testClass()
 obj.sum(5, 6)
 '''
 
-# Example with constructor
+# Example with constructor and default value
+
 '''
 class student:
-    def __init__(self, name, sub1, sub2, sub3):
+    def __init__(self, name="DEMO", sub1=100, sub2=100, sub3=100):
         self.name = name
         self.sub1 = sub1
         self.sub2 = sub2
@@ -43,12 +44,20 @@ class student:
     def grd(self):
         if (self.sub1+self.sub2+self.sub3) >= 240:
             print("Grade: A+")
-        if (self.sub1+self.sub2+self.sub3) < 120:
+        elif (self.sub1+self.sub2+self.sub3) < 120:
             print("Grade: F")
 
 
 s0 = student("aaa", 80, 90, 95)
 s1 = student("bbb", 30, 35, 28)
-print(s0.tnm(), s0.grd())
-print(s1.tnm(), s1.grd())
+s2 = student()
+
+s0.tnm()
+s0.grd()
+
+s1.tnm()
+s1.grd()
+
+s2.tnm()
+s2.grd()
 '''
