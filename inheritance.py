71421a86085f7194d5dfd04c04663db2c9796f58
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-16 09:52:13 +0600

    commit from office

--- inheritance.py
+++ inheritance.py
@@ -1,5 +1,5 @@
 # Python inheritance
-
+'''
 class parentClass:
     def __init__(self, num1=500, num2=100):
         self.number1 = num1
@@ -23,3 +23,29 @@ object1.sum()
 
 object2 = parentClass()
 object2.sum()
+'''
+
+'''
+class parentClass:
+    def __init__(self, num1=100, num2=200):
+        self.number1 = num1
+        self.number2 = num2
+
+    def sum(self):
+        return self.number1+self.number2
+
+
+class childClass(parentClass):
+    def __init__(self, num1, num2):
+        super().__init__(num1, num2)
+
+    def printFromChild(self):
+        print(parentClass.sum(self))
+
+
+cobj = childClass(500, 600)
+cobj.printFromChild()
+
+pobj = parentClass()
+print(pobj.sum())
+'''
